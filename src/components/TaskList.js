b8a2077fd3f1b44049457b4625833b45c8e2ea3b
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-15 23:11:34 +0200

    displaying tasks in both sections

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,18 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
-  const tasks = props.tasks.map((task) => (
+  const active = props.tasks.filter((task) => task.active);
+  const done = props.tasks.filter((task) => !task.active);
+  console.log(active, done);
+  const activeTasks = active.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      delete={props.delete}
+      change={props.change}
+    />
+  ));
+  const doneTasks = done.map((task) => (
     <Task
       key={task.id}
       task={task}
@@ -14,11 +25,23 @@ const TaskList = (props) => {
     <>
       <div className="active">
         <h1>Zadania do zrobienia</h1>
-        {tasks}
+        {activeTasks.length > 0 ? (
+          activeTasks
+        ) : (
+          <p>brak zadań, ale jesteś szczęśliwym człowiekiem!</p>
+        )}
       </div>
       <hr />
       <div className="done">
-        <h3>Zadania zrobione (0)</h3>
+        <h3>
+          Zadania zrobione <em>({done.length})</em>
+        </h3>
+        {done.length > 5 && (
+          <span style={{ fontSize: "12px" }}>
+            wyświetlonych jest jedynie 5 ostatnich zadań
+          </span>
+        )}
+        {doneTasks.slice(0, 5)}
       </div>
     </>
   );
