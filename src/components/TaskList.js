c3aac3849da8460554dd6ab15828333d9e97d868
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-22 21:05:04 +0200

    sorting of added tasks by date in both sections

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,35 @@ import Task from "./Task";
 const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.active);
   const done = props.tasks.filter((task) => !task.active);
-  console.log(active, done);
+  // Pierwszy sposób na sort
+  // done.sort((a, b) => b.finishDate - a.finishDate);
+
+  // Drugi sposób sortowania
+  if (done.length >= 2) {
+    done.sort((a, b) => {
+      if (a.finishDate < b.finishDate) {
+        return 1;
+      }
+      if (a.finishDate > b.finishDate) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+  // 0
+  // < 0
+  // > 0
+  if (active.length >= 2) {
+    active.sort((a, b) => {
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    });
+  }
+  // console.log(active, done);
   const activeTasks = active.map((task) => (
     <Task
       key={task.id}
